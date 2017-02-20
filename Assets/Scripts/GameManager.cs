c165a5db332cecc85b536a7ae98c77d2300582c5
Ludovic <ludovicaubut@hotmail.com>
2017-02-20 03:10:12 -0500

    Corrected the player instantiation rotation.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.Networking;
 
 public class GameManager : NetworkBehaviour {
 
+	public GameObject spawnPrefab;
 	private GameObject[] planetList;
 	[SyncVar]
 	private int currentPlanet;
@@ -25,6 +26,20 @@ public class GameManager : NetworkBehaviour {
 
 	public void RandomizePlanet () {
 		currentPlanet = Random.Range (0, planetList.Length - 1);
+		GameObject[] spawns = GameObject.FindGameObjectsWithTag ("Spawn");
+		MoveSpawn (spawns [0], new Vector3 (10.0f, 0.0f, 0.0f));
+		MoveSpawn (spawns [1], new Vector3 (-10.0f, 0.0f, 0.0f));
+		MoveSpawn (spawns [2], new Vector3 (0.0f, 10.0f, 0.0f));
+		MoveSpawn (spawns [3], new Vector3 (0.0f, -10.0f, 0.0f));
+		MoveSpawn (spawns [4], new Vector3 (0.0f, 0.0f, 10.0f));
+		MoveSpawn (spawns [5], new Vector3 (0.0f, 0.0f, -10.0f));
+		MoveSpawn (spawns [6], new Vector3 (10.0f, 10.0f, 0.0f));
+		MoveSpawn (spawns [7], new Vector3 (-10.0f, -10.0f, 0.0f));
+	}
+
+	private void MoveSpawn(GameObject spawn, Vector3 modifier) {
+		spawn.transform.position = GetPlanet ().transform.position + modifier;
+		spawn.transform.rotation = Quaternion.LookRotation (spawn.transform.position - GetPlanet ().transform.position + new Vector3(0.0f, -90.0f, 0.0f));
 	}
 
 	public GameObject GetPlanet() {
